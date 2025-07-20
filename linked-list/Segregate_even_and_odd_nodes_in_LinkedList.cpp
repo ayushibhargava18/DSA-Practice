7fe9d3365dec6770b09dbec9efba0010879c1af2
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-07-20 14:46:58 +0530

    Create Segregate_even_and_odd_nodes_in_LinkedList.cpp

---
+++ linked-list/Segregate_even_and_odd_nodes_in_LinkedList.cpp
@@ -0,0 +1,74 @@
+
+#include <iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node* next;
+};
+
+
+void insertatend(node*& head, int x){
+    node* newnode = new node();
+    newnode->data=x;
+    if (head==NULL){
+        head=newnode;
+        newnode->next=NULL;
+    }
+    else{
+        node* temp=head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=newnode;
+        newnode->next=NULL;
+    }
+}
+
+void display(node*& head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"\n";
+        temp=temp->next;
+    }
+}
+
+void oddeven(node*& head ){
+    node* temp=head;
+    node* oddhead=NULL;
+    node* evenhead=NULL;
+    
+    while(temp!=NULL){
+        if(temp->data&1){
+            insertatend(oddhead, temp->data);
+        }
+        else{
+            insertatend(evenhead, temp->data);
+        }
+        temp=temp->next;
+    }
+    
+    node* eventemp=evenhead;
+    while(eventemp->next!=NULL){
+        eventemp=eventemp->next;
+    }
+    eventemp->next=oddhead;
+    display(evenhead);
+}
+
+
+int main()
+{
+    node* head=NULL;
+
+    insertatend(head, 1);
+    insertatend(head, 2);
+    insertatend(head, 3);
+    insertatend(head, 4);
+    insertatend(head, 5);
+    insertatend(head, 6);
+    display(head);
+    
+    cout<<"after: \n";
+    oddeven(head);
+}
