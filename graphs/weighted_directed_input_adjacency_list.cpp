b62b7f6a500efc40b81bbf6d1d97ae1134d24cfe
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-07-30 18:10:59 +0530

    Create weighted_directed_input_adjacency_list.cpp

---
+++ graphs/weighted_directed_input_adjacency_list.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    int u, v, wt;
+    int adj[n+1][n+1]={0};
+    for(int i=0; i<m;i++){
+        
+        cin>>u>>v>>wt;
+        
+        adj[u][v]=wt;
+    }
+    for(int k=1; k<=n;k++){
+        for(int i=1;i<=n;i++){
+            cout<<adj[k][i];
+        }
+    cout<<"\n";
+        
+    }
+    
+
+    return 0;
+}
