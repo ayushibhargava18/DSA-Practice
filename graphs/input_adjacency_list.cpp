92f52b686c33b64d332b583ddd0f8b4c73e79321
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-07-30 17:42:07 +0530

    Create input_adjacency_list.cpp

---
+++ graphs/input_adjacency_list.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    
+    vector<int> adj[n+1];
+    
+    for(int i=0;i<m;i++){
+        int u, v;
+        cin>>u>>v;
+        adj[v].push_back(u);
+        adj[u].push_back(v);
+        
+    }
+    
+    for(int i=0; i<n;i++){
+        cout << i << ": ";
+        for(int j : adj[i]){
+            cout<<j;
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
