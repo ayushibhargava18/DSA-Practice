9800d260ccf50627567d4b036c4e76d55c880472
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-09-14 16:44:43 +0530

    Update bubble_sort_vector.cpp

--- searching-sorting/bubble_sort_vector.cpp
+++ searching-sorting/bubble_sort_vector.cpp
@@ -1,1 +1,36 @@
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class test{
+    public:
+        void bubble(vector<int>& vec){
+            for(int i=0; i<vec.size();i++){
+                for(int j=0;j<vec.size()-1;j++){
+                    if(vec[j]>vec[j+1]){
+                        int temp=vec[j];
+                        vec[j]=vec[j+1];
+                        vec[j+1]=temp;
+                    }
+                    else{
+                        continue;
+                    }
+                }
+            }
+            
+            for(int k=0;k<vec.size();k++){
+                cout<<" "<<vec[k];
+            }
+        }
+};
+
+int main()
+{
+    vector<int> vec={13,46,24,52,20,9};
+    test obj;
+    obj.bubble(vec);
+
+    return 0;
+}
