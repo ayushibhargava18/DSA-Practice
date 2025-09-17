4e55f9e8e474d583d91df332046e1715964b3e07
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-09-17 17:12:46 +0530

    Update Maximum-Product-Subarray.cpp

--- array/Maximum-Product-Subarray.cpp
+++ array/Maximum-Product-Subarray.cpp
@@ -3,9 +3,16 @@ public:
     int maxProduct(vector<int>& nums) {
         int product=nums[0];
         int current=nums[0];
+        int minproduct=nums[0];
 
         for(int i=1; i<nums.size();i++){
+            if(nums[i]<0){
+                swap(minproduct, current);
+            }
+
             current=max(nums[i], current*nums[i]);
+            minproduct=min(nums[i], minproduct*nums[i]);
+
             product=max(product, current);
         }
         return product;
