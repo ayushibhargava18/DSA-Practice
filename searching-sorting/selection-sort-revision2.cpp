5a4baae6e3a7aef8bba3172853e822757dd7bfdb
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-09-23 15:27:23 +0530

    Update selection-sort-revision2.cpp

--- searching-sorting/selection-sort-revision2.cpp
+++ searching-sorting/selection-sort-revision2.cpp
@@ -1,1 +1,41 @@
 
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class test{
+    public:
+        void selection(vector<int>& vec){
+            for(int i=0; i<vec.size();i++){
+                int mini=i;
+                
+                for(int j=i+1; j<vec.size(); j++){
+                    if(vec[j]<vec[mini]){
+                        mini=j;
+                    }
+                }
+                swap(vec[mini], vec[i]);
+            }
+        }
+            
+        void display(vector<int> vec){
+            for(int i=0; i<vec.size(); i++){
+                cout<<vec[i]<< " ";
+            }
+        }
+};
+
+
+
+int main() {
+    test obj;
+    vector<int> vec={3, 6, 2, 8, 1};
+    obj.selection(vec);
+    obj.display(vec);
+
+    
+
+    return 0;
+}
