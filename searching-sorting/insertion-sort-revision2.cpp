da7d14648ffcf0d77eeaeeeaccf89ed4ddb4c2d6
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-09-23 15:23:23 +0530

    Update insertion-sort-revision2.cpp

--- searching-sorting/insertion-sort-revision2.cpp
+++ searching-sorting/insertion-sort-revision2.cpp
@@ -1,1 +1,39 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+class test{
+    public:
+        void insertion(vector<int>& vec){
+            for(int i=0; i<vec.size(); i++){
+                int key=vec[i];
+                int j=i-1;
+                
+                while(j>=0 && vec[j]>key){
+                    vec[j+1]=vec[j];
+                    j--;
+                }
+                vec[j+1]=key;
+            }
+        }
+        void display(vector<int> vec){
+            for(int i=0; i<vec.size(); i++){
+                cout<<vec[i]<< " ";
+            }
+        }
+};
+
+
+
+int main() {
+    test obj;
+    vector<int> vec={3, 6, 2, 8, 1};
+    obj.insertion(vec);
+    obj.display(vec);
+
+    
+
+    return 0;
+}
