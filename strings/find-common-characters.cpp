8610354b0c78596e4b661b9c106e0a279d36ad04
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-09-24 16:55:51 +0530

    Update find-common-characters.cpp

--- strings/find-common-characters.cpp
+++ strings/find-common-characters.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        
+        unordered_map<char, int> freq;
+        vector<string> vec;
 
+        for(char c:words[0]){
+            freq[c]++;
+        }
+
+        for(int i=1; i<words.size(); i++){
+            unordered_map<char, int> temp;
+            for(char c:words[i]){
+                temp[c]++;
+            }
+            for(auto& j : freq){
+                if(temp.find(j.first)==temp.end()){
+                    j.second=0;
+                } 
+                else{
+                    j.second=min(j.second, temp[j.first]);
+                }
+            }
+
+        }
+        for(auto it:freq){
+            for(int i=0; i<it.second; i++){
+                vec.push_back(string(1, it.first));
+            }
+        }
+        return vec;
+    }
+    
+};
