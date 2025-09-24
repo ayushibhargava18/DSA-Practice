4236f411815d2e55c4676a0ab3773667eb61b4a4
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-09-24 17:21:35 +0530

    Create selection-sort-revision-4.cpp

---
+++ searching-sorting/selection-sort-revision-4.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class test{
+    public:
+        void selection(vector<int>& vec){
+            for(int i=0; i<vec.size()-1;i++){
+                int mini = i;
+                
+                for(int j=i+1; j<vec.size();j++){
+                    if(vec[mini]>vec[j]){
+                        mini=j;
+                    }
+                }
+                swap(vec[mini], vec[i]);
+                
+            }
+            
+        }
+        void display(vector<int> vec){
+            for(int i=0; i<vec.size(); i++){
+                cout<<vec[i]<< " ";
+            }
+        }
+    
+};
+
+int main()
+{
+    test obj;
+    vector<int> vec={3, 6, 2, 8, 1};
+    obj.selection(vec);
+    obj.display(vec);
+    return 0;
+}
