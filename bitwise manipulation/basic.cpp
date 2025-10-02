8b74d60d98c64abad4c78e6528599923f674cb17
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-10-02 12:13:54 +0530

    Create basic.cpp

---
+++ bitwise manipulation/basic.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<(5<<3);
+    cout<<"\n"<<(5>>2);
+    cout<<"\n"<<(5^2);
+    cout<<"\n"<<(5&2);
+    cout<<"\n"<<(5|2);
+    cout<<"\n"<<~(5);
+    return 0;
+}
