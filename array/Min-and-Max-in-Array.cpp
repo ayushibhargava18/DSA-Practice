2fae403b0e5ee88a77b6947f5e3c54da65abe198
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-11-06 19:59:37 +0530

    Create Min-and-Max-in-Array.cpp

---
+++ array/Min-and-Max-in-Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> getMinMax(vector<int> &arr) {
+        // code here
+        int mini=arr[0];
+        int maxi=arr[0];
+        
+        
+        for(int i=1; i<arr.size(); i++){
+            if(arr[i]<mini){
+                mini=arr[i];
+            }
+            if(arr[i]>maxi){
+                maxi=arr[i];
+            }
+        }
+        
+        return {mini, maxi};
+    }
+};
