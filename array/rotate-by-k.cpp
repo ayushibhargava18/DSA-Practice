9a7f586034431326ea0d8ac9367d9000a7af3b2e
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-11-07 19:23:35 +0530

    Create rotate-by-k.cpp

---
+++ array/rotate-by-k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    void rotate(vector<int>& arr, int k) {
+        int n=arr.size();
+        k=k%n;
+        reverse(arr, 0, n-1);
+        reverse(arr, 0, k-1);
+        reverse(arr, k, n-1);
+    }
+    
+    void reverse(vector<int>& arr, int start, int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+};
