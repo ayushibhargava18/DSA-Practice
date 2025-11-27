ab931f1abd85391c3fe45a37849b7a73d56e8f2b
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-11-27 18:39:53 +0530

    Create contians-duplicate.cpp

---
+++ array/contians-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> map;
+
+        for(int i=0;i<nums.size();i++){
+            map[nums[i]]++;
+        }
+
+        for(auto m : map){
+            if(m.second>1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
