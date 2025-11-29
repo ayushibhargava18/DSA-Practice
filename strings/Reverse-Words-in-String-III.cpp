ac2e4276f164bf751a279fc2f78369cdc6ce665c
Ayushi Bhargava <115579899+ayushibhargava18@users.noreply.github.com>
2025-11-29 22:41:31 +0530

    Create Reverse-Words-in-String-III.cpp

---
+++ strings/Reverse-Words-in-String-III.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int start=0;
+
+        for (int i = 0; i<=s.size(); i++) {
+            if (s[i] == ' ' || i == s.size()){
+
+                int left = start;
+                int right = i-1;
+
+                while(left<right){
+                    swap(s[left], s[right]);
+                    left++;
+                    right--;
+                }
+
+                start=i+1;
+            }
+                
+        }
+        return s;
+    }
+};
